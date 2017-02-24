b6382f997eaaa30a0f757e9b1eec7da725c5bdfa
Caitlin Rouille <caitlinbaril@yahoo.com>
2017-02-23 21:08:41 -0500

    finishing touches

--- billLogic.php
+++ billLogic.php
@@ -7,31 +7,32 @@ use DWA\Tools;
 $form = new DWA\Form($_POST);
 
 if ($form->isSubmitted())
-	{
+    {
 
-	// # Validate!
-	$errors = $form->validate(['price' => 'required|numeric', 'split' => 'required|numeric']);
-	if (!$errors)
-		{
-		$split = $_POST['split'];
-		$price = $_POST['price'];
-		$total = $price / $split; // # Divide total price
-		$rounded = round($total); // # Round total price
-		if (isset($_POST['roundBill']))
-			{
-			$results = "Each person owes $" . $rounded;
-			}
-		  else
-			{
-			$results = "Each person owes $" . $total;
-			}
-		}
-	  else
-		{
-		$results = '';
-		}
-	}
+    // # Validate!
+
+    $errors = $form->validate(['price' => 'required|numeric', 'split' => 'required|numeric']);
+    if (!$errors)
+        {
+        $split = $_POST['split'];
+        $price = $_POST['price'];
+        $total = $price / $split; // # Divide total price
+        $rounded = round($total); // # Round total price
+        if (isset($_POST['roundBill']))
+            {
+            $results = "Each person owes $" . $rounded;
+            }
+          else
+            {
+            $results = "Each person owes $" . $total;
+            }
+        }
+      else
+        {
+        $results = '';
+        }
+    }
   else
-	{
-	$results = '';
-	}
+    {
+    $results = '';
+    }
--- index.php
+++ index.php
@@ -2,51 +2,52 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>Caitlin Rouille - a2 Assignment</title>
-  <meta charset='utf-8'>
-<link href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css' rel='stylesheet'>
-<link href='css/styles.css' rel='stylesheet'>
+    <title>Caitlin Rouille - a2 Assignment</title>
+    <meta charset='utf-8'>
+    <link href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css' rel='stylesheet'>
+    <link href='css/styles.css' rel='stylesheet'>
 </head>
 <body>
-<div class="container">
-  <h1>Bill Splitter</h1>
-  <form action="index.php" method="post">
-      <div class="form-group">
-          <label for="split">Split how many ways?</label>
-          <input type="text" name="split" required value="">
-      </div>
-      <div class="form-group">
-          <label for="price">How much was the tab?</label>
-          <input type="text" name="price" required value="">
-      </div>
-      <div class="form-group">
-          <label for="service">How was the service:</label>
-          <select name="service">
-            <option value='choose'>Choose one...</option>
-            <option value='good'>Good</option>
-            <option value='fair'>Fair</option>
-            <option value='poor'>Poor</option>
-          </select>
-      </div>
-      <div class="form-group">
-        <label for="round">Round up?</label>
-        <input type="checkbox" name="roundBill" value="checked"/>
-      </div>
-      <input type="submit" name="btn_submit" value="Calculate Bill">
-  </form>
-  <div class="alert-info mg-top-20">
-    <?=$results?>
-  </div>
-
-  <!--validation -->
-  <div>
-    <?php if(isset($errors) && ($errors!=null)): ?>
-      <div class="alert alert-danger">
-        <?php foreach($errors as $error): ?>
-            <?=$error?><br>
-        <?php endforeach; ?>
-      </div>
-<?php endif; ?>
-</div>
+    <div class="container mg-top-20">
+        <h3>Bill Splitter</h3>
+        <form action="index.php" method="post">
+            <div class="form-group">
+                <label for="split">Split how many ways?</label> <input id="split" name="split" required="" type="text">
+            </div>
+            <div class="form-group">
+                <label for="price">How much was the tab?</label> <input id="price" name="price" required="" type="text">
+            </div>
+            <div class="form-group">
+                <label for="service">How was the service:</label> <select id="service" name="service">
+                    <option value='choose'>
+                        Choose one...
+                    </option>
+                    <option value='good'>
+                        Good
+                    </option>
+                    <option value='fair'>
+                        Fair
+                    </option>
+                    <option value='poor'>
+                        Poor
+                    </option>
+                </select>
+            </div>
+            <div class="form-group">
+                <label for="roundBill">Round up?</label> <input id="roundBill" name="roundBill" type="checkbox" value="checked">
+            </div><input name="btn_submit" type="submit" value="Calculate Bill">
+        </form>
+        <div class="alert-info mg-top-20">
+            <?=$results?>
+        </div>
+    </div><!--validation -->
+    <div>
+        <?php if(isset($errors) && ($errors!=null)): ?>
+        <div class="alert alert-danger">
+            <?php foreach($errors as $error): ?><?=$error?>
+            <br>
+            <?php endforeach; ?>
+        </div><?php endif; ?>
+    </div>
 </body>
 </html>
